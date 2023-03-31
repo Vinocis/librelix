682a3aca93e3468fa381e80fe3e36497d2b473d2
Vinocis <vinicius.alexandre68@gmail.com>
2023-03-31 18:01:21 -0300

    Add Basic Auth config

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,5 +1,10 @@
 import Config
 
+# Configures basic auth plug
+config :librelix, :basic_auth,
+  username: System.get_env("BASIC_AUTH_USER"),
+  password: System.get_env("BASIC_AUTH_PASS")
+
 # config/runtime.exs is executed for all environments, including
 # during releases. It is executed after compilation and before the
 # system starts, so it is typically used to load production configuration
--- config/test.exs
+++ config/test.exs
@@ -14,6 +14,11 @@ config :librelix, LibrElix.Repo,
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
 
+# Configures basic auth plug
+config :librelix, :basic_auth,
+  username: "test",
+  password: "secret"
+
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :librelix, LibrElixWeb.Endpoint,
